886997c74e12ad996bba85ea76057c4adcb7ac48
Katy Taylor <kt17@sanger.ac.uk>
2020-09-14 14:09:27 +0100

    clean up

--- crawler/config/test.py
+++ crawler/config/test.py
@@ -91,10 +91,6 @@ MLWH_DB_RO_PASSWORD = "root"
 MLWH_DB_RW_USER = "root"
 MLWH_DB_RW_PASSWORD = "root"
 
-# flask
-# FLASK_APP=crawler
-# FLASK_ENV=test
-
 # logging config
 LOGGING["loggers"]["crawler"]["level"] = "DEBUG"
 LOGGING["loggers"]["crawler"]["handlers"] = ["colored_stream"]
