757a83294a9a232608e7561ac337eafd4e81659d
Andrew Sparkes <as28@sanger.ac.uk>
2020-09-16 17:23:23 +0100

    Moved update for mlwh into migrations

---
+++ migrations/helpers/mlwh_samples_update_helper.py
@@ -0,0 +1,106 @@
+import logging
+import logging.config
+import time
+from typing import List
+import pymongo
+import sys
+import traceback
+from crawler.constants import (
+    COLLECTION_SAMPLES,
+    FIELD_CREATED_AT,
+    MONGO_DATETIME_FORMAT,
+)
+from crawler.db import (
+    create_mongo_client,
+    get_mongo_collection,
+    get_mongo_db,
+    create_mysql_connection,
+    run_mysql_executemany_query,
+)
+from crawler.helpers import (
+    get_config,
+    map_mongo_doc_to_sql_columns,
+)
+from datetime import datetime
+from crawler.sql_queries import SQL_MLWH_MULTIPLE_INSERT
+
+def valid_datetime_string(s_datetime: str) -> bool:
+    try:
+        dt = datetime.strptime(s_datetime, MONGO_DATETIME_FORMAT)
+        if dt == None:
+            return False
+        return True
+    except Exception as e:
+        print_exception()
+        return False
+
+def print_exception() -> None:
+    print(f'An exception occurred, at {datetime.now()}')
+    e = sys.exc_info()
+    print(e[0]) # exception type
+    print(e[1]) # exception message
+    if e[2]: # traceback
+      traceback.print_tb(e[2], limit=10)
+
+def update_mlwh_with_legacy_samples(config, s_start_datetime: str = "", s_end_datetime: str = "") -> None:
+    print(f"DEBUG: In update method with start datetime {s_start_datetime} and end datetime {s_end_datetime}")
+
+    if not valid_datetime_string(s_start_datetime):
+        print('Aborting run: Start datetime is not in the expected datetime format')
+        return
+
+    if not valid_datetime_string(s_end_datetime):
+        print('Aborting run: End datetime is not in the expected datetime format')
+        return
+
+    start_datetime = datetime.strptime(s_start_datetime, MONGO_DATETIME_FORMAT)
+    end_datetime = datetime.strptime(s_end_datetime, MONGO_DATETIME_FORMAT)
+
+    if start_datetime > end_datetime:
+        print("Aborting run: End datetime must be greater than start datetime")
+        return
+
+    print(f"DEBUG: In update method with start datetime {start_datetime} and end datetime {end_datetime}")
+
+    try:
+        mongo_docs_for_sql = []
+        number_docs_found  = 0
+
+        # open connection mongo
+        with create_mongo_client(config) as client:
+            mongo_db = get_mongo_db(config, client)
+
+            samples_collection = get_mongo_collection(mongo_db, COLLECTION_SAMPLES)
+
+            print("Selecting Mongo samples")
+
+            # this should take everything from the cursor find into RAM memory (assuming you have enough memory)
+            mongo_docs = list(
+                samples_collection.find(
+                    {
+                        FIELD_CREATED_AT:{
+                            '$gte': start_datetime,
+                            '$lte': end_datetime
+                        }
+                    }
+                )
+            )
+            number_docs_found = len(mongo_docs)
+            print(f"{number_docs_found} documents found in the mongo database between these timestamps")
+
+            # convert mongo field values into MySQL format
+            for doc in mongo_docs:
+                mongo_docs_for_sql.append(map_mongo_doc_to_sql_columns(doc))
+
+        if number_docs_found > 0:
+            print(f"Updating MLWH database for {len(mongo_docs_for_sql)} sample documents")
+            # create connection to the MLWH database
+            with create_mysql_connection(config, True) as mlwh_conn:
+
+                    # execute sql query to insert/update timestamps into MLWH
+                    run_mysql_executemany_query(mlwh_conn, SQL_MLWH_MULTIPLE_INSERT, mongo_docs_for_sql)
+        else:
+            print("No documents found for this timestamp range, nothing to insert or update in MLWH")
+
+    except Exception as e:
+        print_exception()
