2b50e2fe46be02fe0e0caddd858d3be71635c330
Katy Taylor <kt17@sanger.ac.uk>
2020-09-24 14:31:11 +0100

    compact to one line

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from unittest.mock import patch
-from unittest.mock import MagicMock
+from unittest.mock import (patch, MagicMock)
 import pytest
 
 from pymongo import MongoClient
