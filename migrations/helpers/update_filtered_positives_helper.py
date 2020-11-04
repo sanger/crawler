256c952c73cb5a69ca71cadc0c685de006d700e0
Christopher Friend <christopher.friend@tessella.com>
2020-11-04 16:11:07 +0000

    Add comments on expected implementation

--- migrations/helpers/update_filtered_positives_helper.py
+++ migrations/helpers/update_filtered_positives_helper.py
@@ -1,2 +1,6 @@
 def update_filtered_positives(config):
-    # TODO implement
+    # Get pending plate barcodes from DART - no way to do this yet
+    # Pull all RESULT=positive samples from mongo with these plate barcodes - can probably do this. Get everything structured from mongo as we pass it to mongo
+    # Re-determine whether filtered-positive - just pass what we get from mongo through the identifier
+    # Update filtered-positive version etc. in mongo and MLWH
+    # Re-upload well properties to DART - call insert_plates_and_wells_from_docs_into_dart?
