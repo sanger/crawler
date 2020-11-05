305679441114979bfc39bccfa059c4fd34594674
Christopher Friend <christopher.friend@tessella.com>
2020-11-05 15:42:17 +0000

    Add some tests for update_filtered_positives method

--- tests/migrations/test_update_filtered_positives.py
+++ tests/migrations/test_update_filtered_positives.py
@@ -22,8 +22,8 @@ def dart_conn():
 
 @pytest.fixture
 def print_exception():
-    with patch('migrations.helpers.update_filtered_positives_helper.print_exception') as mock_print:
-        yield mock_print
+    with patch('migrations.helpers.update_filtered_positives_helper.print_exception') as mock_print_exception:
+        yield mock_print_exception
 
 @pytest.fixture
 def mongo_client():
@@ -39,6 +39,11 @@ def mongo_db():
 def mongo_collection():
     with patch('migrations.helpers.update_filtered_positives_helper.get_mongo_collection') as mock_collection:
         yield mock_collection
+
+@pytest.fixture
+def print_status():
+    with patch('migrations.helpers.update_filtered_positives_helper.print_processing_status') as mock_print_status:
+        yield mock_print_status
 
 # ----- test pending_plate_barcodes_from_dart method -----
 
@@ -63,13 +68,13 @@ def test_pending_plate_barcodes_from_dart_handles_error_executing_statement(conf
     print_exception.assert_called_once()
 
 def test_pending_plate_barcodes_from_dart_handles_error_committing(config, dart_conn, print_exception):
-    dart_conn().cursor().commit = MagicMock(side_effect = Exception('Boom!'))
+    dart_conn().cursor().commit.side_effect = Exception('Boom!')
     pending_plate_barcodes_from_dart(config)
     print_exception.assert_called_once()
 
 def test_pending_plate_barcodes_from_dart_returns_expected_plate_barcodes(config, dart_conn):
     expected_plate_barcodes = ['ABC123', '123ABC', 'abcdef']
-    dart_conn().cursor().commit = MagicMock(return_value = expected_plate_barcodes)
+    dart_conn().cursor().commit.return_value = expected_plate_barcodes
     result = pending_plate_barcodes_from_dart(config)
 
     dart_conn().cursor().execute.assert_called_once_with('{CALL dbo.plDART_PendingPlates}')
@@ -113,3 +118,39 @@ def test_positive_result_samples_from_mongo_returns_expected_samples(config, mon
     mongo_collection().find.return_value = expected_samples
     result = positive_result_samples_from_mongo(config, plate_barcodes)
     assert result == expected_samples
+
+# ----- test update_filtered_positives method -----
+
+# TODO - add more tests as more of the method is implemented
+
+def test_update_filtered_positives_catches_error_fetching_from_dart(config, dart_conn, print_exception, print_status):
+    dart_conn.side_effect = ValueError('Boom!')
+    update_filtered_positives(config)
+
+    print_exception.assert_called_once()
+    print_status.assert_called_once_with(False, False, False)
+    # TODO - test no database update methods are called?
+
+def test_update_filtered_positives_aborts_with_no_plates_fetched_from_dart(config, dart_conn, print_status):
+    dart_conn().cursor().commit.return_value = []
+    update_filtered_positives(config)
+
+    print_status.assert_called_once_with(False, False, False)
+    # TODO - test no database update methods are called?
+
+def test_update_filtered_positives_catches_error_fetching_from_mongo(config, dart_conn, mongo_client, print_exception, print_status):
+    dart_conn().cursor().commit.return_value = ['ABC123']
+    mongo_client.side_effect = NotImplementedError('Boom!')
+    update_filtered_positives(config)
+
+    print_exception.assert_called_once()
+    print_status.assert_called_once_with(False, False, False)
+    # TODO - test no database update methods are called?
+
+def test_update_filtered_positives_aborts_with_no_positive_samples_fetched_from_mongo(config, dart_conn, mongo_collection, print_status):
+    dart_conn().cursor().commit.return_value = ['ABC123']
+    mongo_collection().find.return_value = []
+    update_filtered_positives(config)
+
+    print_status.assert_called_once_with(False, False, False)
+    # TODO - test no database update methods are called?
