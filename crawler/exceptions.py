ec41aec19bc96725b76d4c857e9137f37a0b54e3
Christopher Friend <christopher.friend@tessella.com>
2020-11-09 13:31:26 +0000

    Add new DartStateError error type

--- crawler/exceptions.py
+++ crawler/exceptions.py
@@ -17,3 +17,17 @@ class CentreFileError(Error):
             return f"CentreFileError: {self.message}"
         else:
             return f"CentreFileError: {default_message}"
+
+class DartStateError(Error):
+    """Raised when the state of plate in DART is not a permitted value."""
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __str__(self):
+        default_message = "Error with the DART plate's state"
+
+        if self.message:
+            return f"DartStateError: {self.message}"
+        else:
+            return f"DartStateError: {default_message}"
