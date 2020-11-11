4330921206c8a38bf7fc2b87df528f926851248f
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2020-11-11 20:26:44 +0000

    Appease mypy

--- crawler/file_processing.py
+++ crawler/file_processing.py
@@ -593,9 +593,9 @@ class CentreFile:
             try:
                 cursor = sql_server_connection.cursor()
 
-                for plate_barcode, samples in groupby_transform(
+                for plate_barcode, samples in groupby_transform(  # type:ignore
                     docs_to_insert, lambda x: x[FIELD_PLATE_BARCODE]
-                ):  # type:ignore
+                ):
                     try:
                         plate_state = self.create_dart_plate_if_doesnt_exist(cursor, plate_barcode)
                         if plate_state == DART_STATE_PENDING:
