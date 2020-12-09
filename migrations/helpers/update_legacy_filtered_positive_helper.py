dc11618ac2db276dea1fa7ca281a352067253de2
Alexander Perrin <ap40@sanger.ac.uk>
2020-12-09 14:21:57 +0000

    Add cherrypicking function to helper

--- migrations/helpers/update_legacy_filtered_positive_helper.py
+++ migrations/helpers/update_legacy_filtered_positive_helper.py
@@ -37,7 +37,7 @@ def all_mongo_samples(config: ModuleType):
         None
     
     Returns:
-        Boolean -- Filtered positive fields exist
+        List[Dict] -- All samples from Mongo
     """
     with create_mongo_client(config) as client:
         mongo_db = get_mongo_db(config, client)
@@ -45,3 +45,82 @@ def all_mongo_samples(config: ModuleType):
 
         return list(samples_collection.find({}))
 
+
+def get_cherrypicked_samples(
+    config: ModuleType,
+    root_sample_ids: List[str],
+    plate_barcodes: List[str],
+    chunk_size: int = 50000,
+) -> Optional[DataFrame]:
+    """Find which samples have been cherrypicked using MLWH & Events warehouse.
+    Returns dataframe with 4 columns: those needed to uniquely identify the sample resulting
+    dataframe only contains those samples that have been cherrypicked (those that have an entry
+    for the relevant event type in the event warehouse)
+
+    Args:
+        root_sample_ids (List[str]): [description]
+        plate_barcodes (List[str]): [description]
+        chunk_size (int, optional): [description]. Defaults to 50000.
+
+    Returns:
+        DataFrame: [description]
+    """
+    try:
+        logger.debug("Getting cherry-picked samples from MLWH")
+
+        # Create an empty DataFrame to merge into
+        concat_frame = pd.DataFrame()
+
+        chunk_root_sample_ids = [
+            root_sample_ids[x : (x + chunk_size)] for x in range(0, len(root_sample_ids), chunk_size)  # noqa:E203
+        ]
+
+        sql_engine = sqlalchemy.create_engine(
+            (
+                f"mysql+pymysql://{config.MLWH_DB_RO_USER}:{config.MLWH_DB_RO_PASSWORD}"  # type: ignore
+                f"@{config.MLWH_DB_HOST}"  # type: ignore
+            ),
+            pool_recycle=3600,
+        )
+        db_connection = sql_engine.connect()
+
+        ml_wh_db = config.MLWH_DB_DBNAME  # type: ignore
+        events_wh_db = config.EVENTS_WH_DB  # type: ignore
+
+        for chunk_root_sample_id in chunk_root_sample_ids:
+            sql = (
+                f"SELECT mlwh_sample.description as `{FIELD_ROOT_SAMPLE_ID}`, mlwh_stock_resource.labware_human_barcode as `{FIELD_PLATE_BARCODE}`"  # noqa: E501
+                f",mlwh_sample.phenotype as `Result_lower`, mlwh_stock_resource.labware_coordinate as `{FIELD_COORDINATE}`"  # noqa: E501
+                f" FROM {ml_wh_db}.sample as mlwh_sample"
+                f" JOIN {ml_wh_db}.stock_resource mlwh_stock_resource ON (mlwh_sample.id_sample_tmp = mlwh_stock_resource.id_sample_tmp)"  # noqa: E501
+                f" JOIN {events_wh_db}.subjects mlwh_events_subjects ON (mlwh_events_subjects.friendly_name = sanger_sample_id)"  # noqa: E501
+                f" JOIN {events_wh_db}.roles mlwh_events_roles ON (mlwh_events_roles.subject_id = mlwh_events_subjects.id)"  # noqa: E501
+                f" JOIN {events_wh_db}.events mlwh_events_events ON (mlwh_events_roles.event_id = mlwh_events_events.id)"  # noqa: E501
+                f" JOIN {events_wh_db}.event_types mlwh_events_event_types ON (mlwh_events_events.event_type_id = mlwh_events_event_types.id)"  # noqa: E501
+                f" WHERE mlwh_sample.description IN %(root_sample_ids)s"
+                f" AND mlwh_stock_resource.labware_human_barcode IN %(plate_barcodes)s"
+                " AND mlwh_events_event_types.key = 'cherrypick_layout_set'"
+                " GROUP BY mlwh_sample.description, mlwh_stock_resource.labware_human_barcode, mlwh_sample.phenotype, mlwh_stock_resource.labware_coordinate"  # noqa: E501
+            )
+
+            frame = pd.read_sql(
+                sql,
+                db_connection,
+                params={
+                    "root_sample_ids": tuple(chunk_root_sample_id),
+                    "plate_barcodes": tuple(plate_barcodes),
+                },
+            )
+
+            # drop_duplicates is needed because the same 'root sample id' could pop up in two different batches,
+            # and then it would retrieve the same rows for that root sample id twice
+            # do reset_index after dropping duplicates to make sure the rows are numbered in a way that makes sense
+            concat_frame = concat_frame.append(frame).drop_duplicates().reset_index(drop=True)
+
+        return concat_frame
+    except Exception as e:
+        logger.error("Error while connecting to MySQL")
+        logger.exception(e)
+        return None
+    finally:
+        db_connection.close()
