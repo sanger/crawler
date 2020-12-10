95688f7de6cf012088a4e0badbd39772997f5f6c
Christopher Friend <christopher.friend@tessella.com>
2020-12-10 13:46:25 +0000

    Re-add FilteredPositiveIdentifier version 1

--- crawler/filtered_positive_identifier.py
+++ crawler/filtered_positive_identifier.py
@@ -47,6 +47,49 @@ class FilteredPositiveIdentifierV0(FilteredPositiveIdentifier):
         return self.result_regex.match(sample[FIELD_RESULT]) is not None
 
 
+class FilteredPositiveIdentifierV1(FilteredPositiveIdentifier):
+    result_regex = re.compile(f"^{POSITIVE_RESULT_VALUE}", re.IGNORECASE)
+    root_sample_id_control_regex = re.compile("^CBIQA_")
+    ct_value_limit = decimal.Decimal(30)
+    d128_context = create_decimal128_context()
+
+    def version(self) -> str:
+        return FILTERED_POSITIVE_VERSION_1
+
+    def is_positive(self, sample: Sample) -> bool:
+        """Determines whether a sample is a filtered positive.
+
+        Arguments:
+            sample {Sample} -- information on a single sample
+
+        Returns:
+            {bool} -- whether the sample is a filtered positive
+        """
+        if self.result_regex.match(sample[FIELD_RESULT]) is None:
+            return False
+
+        if self.root_sample_id_control_regex.match(sample[FIELD_ROOT_SAMPLE_ID]) is not None:
+            return False
+
+        ch1_cq = sample.get(FIELD_CH1_CQ)
+        ch2_cq = sample.get(FIELD_CH2_CQ)
+        ch3_cq = sample.get(FIELD_CH3_CQ)
+
+        if ch1_cq is None and ch2_cq is None and ch3_cq is None:
+            return True
+
+        with decimal.localcontext(self.d128_context):
+            # type check before attempting to convert to decimal
+            if ch1_cq is not None and ch1_cq.to_decimal() <= self.ct_value_limit:
+                return True
+            elif ch2_cq is not None and ch2_cq.to_decimal() <= self.ct_value_limit:
+                return True
+            elif ch3_cq is not None and ch3_cq.to_decimal() <= self.ct_value_limit:
+                return True
+            else:
+                return False
+
+
 class FilteredPositiveIdentifierV2(FilteredPositiveIdentifier):
     result_regex = re.compile(f"^(?:{POSITIVE_RESULT_VALUE}|{LIMIT_OF_DETECTION_RESULT_VALUE})", re.IGNORECASE)
     root_sample_id_control_regex = re.compile("^(?:CBIQA_|QC0|ZZA000)")
--- tests/test_filtered_positive_identifier.py
+++ tests/test_filtered_positive_identifier.py
@@ -14,6 +14,7 @@ from crawler.filtered_positive_identifier import (
     FILTERED_POSITIVE_VERSION_1,
     FILTERED_POSITIVE_VERSION_2,
     FilteredPositiveIdentifierV0,
+    FilteredPositiveIdentifierV1,
     FilteredPositiveIdentifierV2,
 )
 
@@ -80,6 +81,96 @@ def test_v0_is_positive_returns_false_result_not_positive():
     # 'limit of detection' result
     sample = positive_sample()
     sample[FIELD_RESULT] = LIMIT_OF_DETECTION_RESULT_VALUE
+    assert identifier.is_positive(sample) is False
+
+
+# ----- tests for FilteredPositiveIdentifierV1 -----
+
+
+def test_v1_version():
+    identifier = FilteredPositiveIdentifierV1()
+    assert identifier.version() == FILTERED_POSITIVE_VERSION_1
+
+
+def test_v1_is_positive_returns_true_matching_criteria():
+    identifier = FilteredPositiveIdentifierV1()
+
+    # expected positive match
+    sample = positive_sample()
+    assert identifier.is_positive(sample) is True
+
+    # case invariant positive match
+    sample = positive_sample()
+    sample[FIELD_RESULT] = "POSITIVE"
+    assert identifier.is_positive(sample) is True
+
+    # 3x mix of ct values
+    sample = positive_sample()
+    sample[FIELD_CH2_CQ] = Decimal128("41.12345678")
+    sample[FIELD_CH3_CQ] = None
+    assert identifier.is_positive(sample) is True
+
+    sample = positive_sample()
+    sample[FIELD_CH1_CQ] = None
+    sample[FIELD_CH3_CQ] = Decimal128("42.12345678")
+    assert identifier.is_positive(sample) is True
+
+    sample = positive_sample()
+    sample[FIELD_CH1_CQ] = Decimal128("40.12345678")
+    sample[FIELD_CH2_CQ] = None
+    assert identifier.is_positive(sample) is True
+
+    # all ct values None
+    sample = positive_sample()
+    sample[FIELD_CH1_CQ] = None
+    sample[FIELD_CH2_CQ] = None
+    sample[FIELD_CH3_CQ] = None
+    assert identifier.is_positive(sample) is True
+
+    # no FIELD_CHX_CQ fields
+    sample = {FIELD_RESULT: POSITIVE_RESULT_VALUE, FIELD_ROOT_SAMPLE_ID: "MCM001"}
+    assert identifier.is_positive(sample) is True
+
+
+def test_v1_is_positive_returns_false_result_not_positive():
+    identifier = FilteredPositiveIdentifierV1()
+
+    # does not conform to regex
+    sample = positive_sample()
+    sample[FIELD_RESULT] = "  positive"
+    assert identifier.is_positive(sample) is False
+
+    # negative result
+    sample = positive_sample()
+    sample[FIELD_RESULT] = "Negative"
+    assert identifier.is_positive(sample) is False
+
+    # void result
+    sample = positive_sample()
+    sample[FIELD_RESULT] = "Void"
+    assert identifier.is_positive(sample) is False
+
+    # 'limit of detection' result
+    sample = positive_sample()
+    sample[FIELD_RESULT] = LIMIT_OF_DETECTION_RESULT_VALUE
+    assert identifier.is_positive(sample) is False
+
+
+def test_v1_is_positive_returns_false_control_sample():
+    identifier = FilteredPositiveIdentifierV1()
+
+    sample = positive_sample()
+    sample[FIELD_ROOT_SAMPLE_ID] = "CBIQA_MCM001"
+    assert identifier.is_positive(sample) is False
+
+
+def test_v1_is_positive_returns_false_all_ct_values_greater_than_30():
+    identifier = FilteredPositiveIdentifierV1()
+
+    sample = positive_sample()
+    sample[FIELD_CH1_CQ] = Decimal128("40.12345678")
+    sample[FIELD_CH2_CQ] = Decimal128("41.12345678")
+    sample[FIELD_CH3_CQ] = Decimal128("42.12345678")
     assert identifier.is_positive(sample) is False
 
 
