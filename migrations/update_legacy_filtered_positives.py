fd2568b1eb8fc143d42ff1476455bccc607c8543
Alexander Perrin <ap40@sanger.ac.uk>
2020-12-11 14:55:54 +0000

    Add splitting function

--- migrations/update_legacy_filtered_positives.py
+++ migrations/update_legacy_filtered_positives.py
@@ -48,6 +48,8 @@ def run(settings_module: str = "") -> None:
 
             cp_samples_df = get_v0_cherrypicked_samples(config, list(root_sample_ids), list(plate_barcodes))
 
+            v0_cherrypicked_samples, unmigrated_samples = split_samples_on_version()
+
             filtered_positive_identifier = FilteredPositiveIdentifier()
             version = filtered_positive_identifier.current_version()
             update_timestamp = datetime.now()
