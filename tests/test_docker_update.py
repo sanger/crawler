2e32e04463b8c9623484348d6aa21f8ee488c9d3
Stephen Inglis <si5@sanger.ac.uk>
2020-12-16 13:34:40 +0000

    Linted.

--- tests/test_docker_update.py
+++ tests/test_docker_update.py
@@ -1,18 +1,18 @@
 def test_docker_updates_correctly():
-    assert True == True
+    assert True
 
 
 def test_docker_updates_correctly_2():
-    assert True == True
+    assert True
 
 
 def test_docker_updates_correctly_3():
-    assert True == True
+    assert True
 
 
 def test_docker_updates_correctly_4():
-    assert True == True
+    assert True
 
 
 def test_docker_updates_correctly_5():
-    assert True == True
+    assert True
