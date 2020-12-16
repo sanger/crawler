889b86a14e3f3c345b6c0b2282d9be6cc1313fff
Alexander Perrin <ap40@sanger.ac.uk>
2020-12-16 16:41:18 +0000

    Add test for full migration success if user chooses to continue

--- tests/migrations/test_update_legacy_filtered_positives.py
+++ tests/migrations/test_update_legacy_filtered_positives.py
@@ -67,18 +67,6 @@ def mock_update_filtered_positive_fields():
 # ----- test migration -----
 
 
-def test_update_legacy_filtered_positives_continues_if_user_continues_with_migration(
-    mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0
-):
-    mock_user_input.return_value = "yes"
-    mock_update_mongo, mock_update_mlwh = mock_helper_database_updates
-
-    update_legacy_filtered_positives.run("crawler.config.integration")
-
-    assert mock_update_mongo.call_count == 3
-    assert mock_update_mlwh.call_count == 1
-
-
 def test_update_legacy_filtered_positives_exception_raised_if_user_cancels_migration(
     mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0
 ):
@@ -163,6 +151,41 @@ def test_update_legacy_filtered_positives_outputs_success(
     mock_split_mongo_samples_by_version, mock_combine_samples = mock_helper_functions
 
     mock_v0_version_set.return_value = False
+    mock_unmigrated_mongo_samples.return_value = [{"plate_barcode": "1"}]
+    mock_extract_required_cp_info.return_value = [["id_1"], ["plate_barcode_1"]]
+    mock_get_cherrypicked_samples_by_date.return_value = pd.DataFrame({"root_sample_id": ["s1", "s2"]})
+    mock_split_mongo_samples_by_version.return_value = {
+        FilteredPositiveIdentifierV0(): [{"plate_barcode": "1"}],
+        FilteredPositiveIdentifierV1(): [{"plate_barcode": "2"}],
+        FilteredPositiveIdentifierV2(): [{"plate_barcode": "3"}],
+    }
+    mock_update_mongo.return_value = True
+    mock_update_mlwh.return_value = True
+
+    mock_combine_samples = [{"plate_barcode": "1"}, {"plate_barcode": "2"}, {"plate_barcode": "3"}]
+
+    update_legacy_filtered_positives.run("crawler.config.integration")
+
+    assert mock_update_filtered_positive_fields.call_count == 3
+    assert mock_update_mongo.call_count == 3
+    assert mock_update_mlwh.call_count == 1
+
+
+def test_update_legacy_filtered_positives_successful_if_user_chooses_to_continue(
+    mock_user_input,
+    mock_v0_version_set,
+    mock_helper_database_updates,
+    mock_query_helper_functions,
+    mock_extract_required_cp_info,
+    mock_helper_functions,
+    mock_update_filtered_positive_fields,
+):
+    mock_update_mongo, mock_update_mlwh = mock_helper_database_updates
+    mock_unmigrated_mongo_samples, mock_get_cherrypicked_samples_by_date = mock_query_helper_functions
+    mock_split_mongo_samples_by_version, mock_combine_samples = mock_helper_functions
+
+    mock_v0_version_set.return_value = True
+    mock_user_input.return_value = "yes"
     mock_unmigrated_mongo_samples.return_value = [{"plate_barcode": "1"}]
     mock_extract_required_cp_info.return_value = [["id_1"], ["plate_barcode_1"]]
     mock_get_cherrypicked_samples_by_date.return_value = pd.DataFrame({"root_sample_id": ["s1", "s2"]})
