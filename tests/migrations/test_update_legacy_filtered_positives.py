f98403d40f615c213e0a8b8291f114be8ce5f6e1
Alexander Perrin <ap40@sanger.ac.uk>
2020-12-16 13:58:28 +0000

    Alter tests for user input

--- tests/migrations/test_update_legacy_filtered_positives.py
+++ tests/migrations/test_update_legacy_filtered_positives.py
@@ -12,18 +12,49 @@ def mock_helper_database_updates():
         with patch("migrations.update_legacy_filtered_positives.update_mlwh_filtered_positive_fields") as mock_update_mlwh:
             yield mock_update_mongo, mock_update_mlwh
 
+
 @pytest.fixture
 def mock_user_input():
     with patch("migrations.update_legacy_filtered_positives.get_input") as mock_user_input:
             yield mock_user_input
 
 
+@pytest.fixture
+def mock_v0_version_set():
+    with patch("migrations.update_legacy_filtered_positives.v0_version_set") as mock_v0_version_set:
+        yield mock_v0_version_set
 # ----- test migration -----
 
 
-def test_exception_raised_if_user_cancels_migration(mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0):
+def test_update_legacy_filtered_positives_continues_if_user_continues_with_migration(
+    mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0):
+        mock_user_input.return_value = "yes"
+        mock_update_mongo, mock_update_mlwh = mock_helper_database_updates
+
+        update_legacy_filtered_positives.run("crawler.config.integration")
+
+        assert mock_update_mongo.call_count == 3
+        assert mock_update_mlwh.call_count == 1
+
+
+def test_update_legacy_filtered_positives_exception_raised_if_user_cancels_migration(
+    mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0
+    ):
     with pytest.raises(Exception):
         mock_user_input.return_value="no"
+        mock_update_mongo, mock_update_mlwh = mock_helper_database_updates
+
+        update_legacy_filtered_positives.run("crawler.config.integration")
+
+        mock_update_mongo.assert_not_called()
+        mock_update_mlwh.assert_not_called()
+
+
+def test_update_legacy_filtered_positives_exception_raised_if_user_enters_invalid_input(
+    mock_user_input, mock_helper_database_updates, filtered_positive_testing_samples_v0
+    ):
+    with pytest.raises(Exception):
+        mock_user_input.return_value = "invalid_input"
         mock_update_mongo, mock_update_mlwh = mock_helper_database_updates
 
         update_legacy_filtered_positives.run("crawler.config.integration")
