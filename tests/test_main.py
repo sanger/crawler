e183fa1a21696375afcfc88f8e70d1224253e8cb
Katy Taylor <kt17@sanger.ac.uk>
2021-01-29 13:52:46 +0000

    Update number of centres as declared by this test to 6, to include Randox

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from crawler.constants import COLLECTION_CENTRES, COLLECTION_IMPORTS, COLLECTION
 from crawler.db.mongo import get_mongo_collection
 from crawler.main import run
 
-NUMBER_CENTRES = 5
+NUMBER_CENTRES = 6
 NUMBER_VALID_SAMPLES = 18
 NUMBER_SAMPLES_ON_PARTIAL_IMPORT = 10
 NUMBER_OF_FILES_PROCESSED = 10
