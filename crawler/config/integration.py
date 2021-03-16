edbed8bcdbd7932fe4b2335554a3f144cdffff4b
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-03-16 16:04:10 +0000

    Changes for supporting more centre filenames

--- crawler/config/integration.py
+++ crawler/config/integration.py
@@ -6,4 +6,3 @@ from crawler.config.test import *
 # In order to perform integration tests, we want to ensure we don't delete our test files, so use a different directory.
 DIR_DOWNLOADED_DATA = "tmp/files/"
 
-MONGO_DB = "crawlerTestDB"
