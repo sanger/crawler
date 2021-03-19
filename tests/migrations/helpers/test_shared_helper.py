6d62d222ce7e6df4c4c96ba691ac63792cae3284
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-03-19 17:24:15 +0000

    Readded some tests

--- tests/migrations/helpers/test_shared_helper.py
+++ tests/migrations/helpers/test_shared_helper.py
@@ -5,23 +5,14 @@ from unittest.mock import Mock, patch
 import numpy as np
 import pandas as pd
 
-from crawler.constants import (
-    FIELD_COORDINATE,
-    FIELD_CREATED_AT,
-    FIELD_LAB_ID,
-    FIELD_MONGODB_ID,
-    FIELD_PLATE_BARCODE,
-    FIELD_RESULT,
-    FIELD_RNA_ID,
-    FIELD_ROOT_SAMPLE_ID,
-    FIELD_UPDATED_AT,
-)
-from migrations.helpers.shared_helper import (
-    extract_required_cp_info,
-    get_cherrypicked_samples,
-    remove_cherrypicked_samples,
-    valid_datetime_string,
-)
+from crawler.constants import (FIELD_COORDINATE, FIELD_CREATED_AT,
+                               FIELD_LAB_ID, FIELD_MONGODB_ID,
+                               FIELD_PLATE_BARCODE, FIELD_RESULT, FIELD_RNA_ID,
+                               FIELD_ROOT_SAMPLE_ID, FIELD_UPDATED_AT)
+from migrations.helpers.shared_helper import (extract_required_cp_info,
+                                              get_cherrypicked_samples,
+                                              remove_cherrypicked_samples,
+                                              valid_datetime_string)
 
 # ----- test helpers -----
 
@@ -115,6 +106,149 @@ def test_remove_cherrypicked_samples():
 
 # ----- get_cherrypicked_samples tests -----
 
+# Test Scenario
+# - Mocking database responses
+# - Only the Sentinel query returns matches (No Beckman)
+# - No chunking: a single query is made in which all matches are returned
+# - No duplication of returned matches
+def test_get_cherrypicked_samples_no_beckman(config):
+    expected = [
+        # Cherrypicking query response
+        pd.DataFrame(["MCM001", "MCM003", "MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0, 1, 2]),
+    ]
+    samples = ["MCM001", "MCM002", "MCM003", "MCM004", "MCM005"]
+    plate_barcodes = ["123", "456"]
+
+    with patch("sqlalchemy.create_engine", return_value=Mock()):
+        with patch("pandas.read_sql", side_effect=expected):
+            returned_samples = get_cherrypicked_samples(config, samples, plate_barcodes)
+            assert returned_samples.at[0, FIELD_ROOT_SAMPLE_ID] == "MCM001"  # type: ignore
+            assert returned_samples.at[1, FIELD_ROOT_SAMPLE_ID] == "MCM003"  # type: ignore
+            assert returned_samples.at[2, FIELD_ROOT_SAMPLE_ID] == "MCM005"  # type: ignore
+
+
+# Test Scenario
+# - Mocking database responses
+# - Only the Sentinel queries return matches (No Beckman)
+# - Chunking: multiple queries are made, with all matches contained in the sum of these queries
+# - No duplication of returned matches
+def test_get_cherrypicked_samples_chunking_no_beckman(config):
+    # Note: This represents the results of three different (Sentinel, Beckman) sets of
+    # database queries, each Sentinel query getting indexed from 0. Do not change the
+    # indices here unless you have modified the behaviour of the query.
+    query_results = [
+        pd.DataFrame(["MCM001"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherrypicking query response
+        pd.DataFrame(["MCM003"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherrypicking query response
+        pd.DataFrame(["MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherrypicking query response
+    ]
+    expected = pd.DataFrame(["MCM001", "MCM003", "MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0, 1, 2])
+
+    samples = ["MCM001", "MCM002", "MCM003", "MCM004", "MCM005"]
+    plate_barcodes = ["123", "456"]
+
+    with patch("sqlalchemy.create_engine", return_value=Mock()):
+        with patch("pandas.read_sql", side_effect=query_results):
+            returned_samples = get_cherrypicked_samples(config, samples, plate_barcodes, 2)
+            pd.testing.assert_frame_equal(expected, returned_samples)
+
+
+# Test Scenario
+# - Actual database responses
+# - Only the Sentinel queries return matches (No Beckman)
+# - Chunking: multiple queries are made, with all matches contained in the sum of these queries
+# - Duplication of returned matches across different chunks: duplicates should be filtered out
+def test_get_cherrypicked_samples_repeat_tests_no_beckman(config, mlwh_sentinel_cherrypicked, event_wh_data):
+    # the following come from MLWH_SAMPLE_STOCK_RESOURCE in test data
+    root_sample_ids = ["root_1", "root_2", "root_3", "root_1"]
+    plate_barcodes = ["pb_1", "pb_2", "pb_3"]
+
+    # root_1 will match 2 samples, but only one of those will match an event (on Sanger Sample Id)
+    # therefore we only get 1 of the samples called 'root_1' back (the one on plate 'pb_1')
+    # this also checks we don't get a duplicate row for root_1 / pb_1, despite it cropped up in 2
+    # different 'chunks'
+    expected_rows = [
+        ["root_1", "pb_1", "positive", "A1"],
+        ["root_2", "pb_2", "positive", "A1"],
+        ["root_3", "pb_3", "positive", "A1"],
+    ]
+    expected_columns = [FIELD_ROOT_SAMPLE_ID, FIELD_PLATE_BARCODE, "Result_lower", FIELD_COORDINATE]
+    expected = pd.DataFrame(np.array(expected_rows), columns=expected_columns, index=[0, 1, 2])
+
+    chunk_size = 2
+    returned_samples = get_cherrypicked_samples(config, root_sample_ids, plate_barcodes, chunk_size)
+    pd.testing.assert_frame_equal(expected, returned_samples)
+
+
+
+# Test Scenario
+# - Mocking database responses
+# - Only the Beckman query returns matches (No Sentinel)
+# - No chunking: a single query is made in which all matches are returned
+# - No duplication of returned matches
+def test_get_cherrypicked_samples_no_sentinel(config):
+    expected = [
+        # Cherrypicking query response
+        pd.DataFrame(["MCM001", "MCM003", "MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0, 1, 2]),
+    ]
+    samples = ["MCM001", "MCM002", "MCM003", "MCM004", "MCM005"]
+    plate_barcodes = ["123", "456"]
+
+    with patch("sqlalchemy.create_engine", return_value=Mock()):
+        with patch("pandas.read_sql", side_effect=expected):
+            returned_samples = get_cherrypicked_samples(config, samples, plate_barcodes)
+            assert returned_samples.at[0, FIELD_ROOT_SAMPLE_ID] == "MCM001"  # type: ignore
+            assert returned_samples.at[1, FIELD_ROOT_SAMPLE_ID] == "MCM003"  # type: ignore
+            assert returned_samples.at[2, FIELD_ROOT_SAMPLE_ID] == "MCM005"  # type: ignore
+
+
+# Test Scenario
+# - Mocking database responses
+# - Only the Beckman queries return matches (No Sentinel)
+# - Chunking: multiple queries are made, with all matches contained in the sum of these queries
+# - No duplication of returned matches
+def test_get_cherrypicked_samples_chunking_no_sentinel(config):
+    # Note: This represents the results of three different (Sentinel, Beckman) sets of
+    # database queries, each Beckman query getting indexed from 0. Do not change the
+    # indices here unless you have modified the behaviour of the query.
+    query_results = [
+        pd.DataFrame(["MCM001"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherypicking query response
+        pd.DataFrame(["MCM003"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherypicking query response
+        pd.DataFrame(["MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0]),  # Cherypicking query response
+    ]
+    expected = pd.DataFrame(["MCM001", "MCM003", "MCM005"], columns=[FIELD_ROOT_SAMPLE_ID], index=[0, 1, 2])
+
+    samples = ["MCM001", "MCM002", "MCM003", "MCM004", "MCM005"]
+    plate_barcodes = ["123", "456"]
+
+    with patch("sqlalchemy.create_engine", return_value=Mock()):
+        with patch("pandas.read_sql", side_effect=query_results):
+            returned_samples = get_cherrypicked_samples(config, samples, plate_barcodes, 2)
+            pd.testing.assert_frame_equal(expected, returned_samples)
+
+
+# Test Scenario
+# - Actual database responses
+# - Only the Beckman queries return matches (No Sentinel)
+# - Chunking: multiple queries are made, with all matches contained in the sum of these queries
+# - Duplication of returned matches across different chunks: duplicates should be filtered out
+def test_get_cherrypicked_samples_repeat_tests_no_sentinel(config, mlwh_beckman_cherrypicked, event_wh_data):
+    # the following come from MLWH_SAMPLE_LIGHTHOUSE_SAMPLE in test data
+    root_sample_ids = ["root_5", "root_6", "root_5"]
+    plate_barcodes = ["pb_4", "pb_5", "pb_6"]
+
+    # root_5 will match 2 samples, but only one of those will match an event (on sample uuid)
+    # therefore we only get 1 of the samples called 'root_5' back (the one on plate 'pb_4')
+    # this also checks we don't get a duplicate row for root_5 / pb_4, despite it cropped up in 2
+    # different 'chunks'
+    expected_rows = [["root_5", "pb_4", "positive", "A1"], ["root_6", "pb_5", "positive", "A1"]]
+    expected_columns = [FIELD_ROOT_SAMPLE_ID, FIELD_PLATE_BARCODE, "Result_lower", FIELD_COORDINATE]
+    expected = pd.DataFrame(np.array(expected_rows), columns=expected_columns, index=[0, 1])
+
+    chunk_size = 2
+    returned_samples = get_cherrypicked_samples(config, root_sample_ids, plate_barcodes, chunk_size)
+    pd.testing.assert_frame_equal(expected, returned_samples)
+
+
 
 # Test Scenario
 # - Mocking database responses
@@ -163,7 +297,7 @@ def test_get_cherrypicked_samples_chunking_sentinel_and_beckman(config):
         pd.DataFrame(
             [
                 "MCM001",  # Sentinel
-                "MCM001",  # Sentinel
+                "MCM001",  # Beckman
                 "MCM002",  # Beckman
             ],
             columns=[FIELD_ROOT_SAMPLE_ID],
@@ -172,7 +306,7 @@ def test_get_cherrypicked_samples_chunking_sentinel_and_beckman(config):
         pd.DataFrame(
             [
                 "MCM003",  # Sentinel
-                "MCM003",  # Sentinel
+                "MCM003",  # Beckman
                 "MCM004",  # Beckman
             ],
             columns=[FIELD_ROOT_SAMPLE_ID],
@@ -181,7 +315,7 @@ def test_get_cherrypicked_samples_chunking_sentinel_and_beckman(config):
         pd.DataFrame(
             [
                 "MCM005",  # Sentinel
-                "MCM005",  # Sentinel
+                "MCM005",  # Beckman
                 "MCM006",  # Beckman
             ],
             columns=[FIELD_ROOT_SAMPLE_ID],
