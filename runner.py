4d81a6b43b56c55beb8d1248f0937492cf551918
Stuart McHattie <sm49@sanger.ac.uk>
2021-06-30 11:34:09 +0100

    Update runner to be used for one-off runs only

--- runner.py
+++ runner.py
@@ -1,9 +1,6 @@
 import argparse
 import logging
 import logging.config
-import time
-
-import schedule
 
 from crawler import main
 
@@ -14,12 +11,6 @@ if __name__ == "__main__":
         description="Parse CSV files from the Lighthouse Labs and store the sample information in MongoDB"
     )
 
-    parser.add_argument(
-        "--scheduled",
-        dest="once",
-        action="store_false",
-        help="start scheduled execution, defaults to running once",
-    )
     parser.add_argument(
         "--sftp",
         dest="sftp",
@@ -44,32 +35,12 @@ if __name__ == "__main__":
         help="process only this centre's CSV files",
     )
 
-    parser.set_defaults(once=True)
     parser.set_defaults(sftp=False)
     parser.set_defaults(keep_files=False)
     parser.set_defaults(add_to_dart=False)
 
     args = parser.parse_args()
 
-    if args.once:
-        main.run(
-            sftp=args.sftp, keep_files=args.keep_files, add_to_dart=args.add_to_dart, centre_prefix=args.centre_prefix
-        )
-    else:
-        print("Scheduled to run every 15 minutes")
-
-        # If a run misses its scheduled time, it queues up. If more than one run is queued up, they execute sequentially
-        # i.e. no parallel processing happens
-        schedule.every(15).minutes.do(
-            main.run, sftp=args.sftp, keep_files=args.keep_files, add_to_dart=args.add_to_dart
-        )
-
-        while True:
-            try:
-                schedule.run_pending()
-                time.sleep(1)
-            except Exception as e:
-                logger.error("There was an exception while running the scheduler")
-                logger.exception(e)
-                # We wait 60 seconds so it wont try to check it again during the same minute
-                time.sleep(60)
+    main.run(
+        sftp=args.sftp, keep_files=args.keep_files, add_to_dart=args.add_to_dart, centre_prefix=args.centre_prefix
+    )
