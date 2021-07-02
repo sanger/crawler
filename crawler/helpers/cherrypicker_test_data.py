e96ba6e908e7eae5d862a36b022768ac9c66807e
Stuart McHattie <sm49@sanger.ac.uk>
2021-07-02 18:06:56 +0100

    Fix further issues from black

--- crawler/helpers/cherrypicker_test_data.py
+++ crawler/helpers/cherrypicker_test_data.py
@@ -6,17 +6,14 @@ import requests
 
 logger = logging.getLogger(__name__)
 
-WELL_COORDS = [
-    "A01","A02","A03","A04","A05","A06","A07","A08","A09","A10","A11","A12",
-    "B01","B02","B03","B04","B05","B06","B07","B08","B09","B10","B11","B12",
-    "C01","C02","C03","C04","C05","C06","C07","C08","C09","C10","C11","C12",
-    "D01","D02","D03","D04","D05","D06","D07","D08","D09","D10","D11","D12",
-    "E01","E02","E03","E04","E05","E06","E07","E08","E09","E10","E11","E12",
-    "F01","F02","F03","F04","F05","F06","F07","F08","F09","F10","F11","F12",
-    "G01","G02","G03","G04","G05","G06","G07","G08","G09","G10","G11","G12",
-    "H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","H11","H12"
-]
 
+def make_coords_list():
+    for column in ["A", "B", "C", "D", "E", "F", "G", "H"]:
+        for row in range(1, 13):
+            yield "%s%02d" % (column, row)
+
+
+WELL_COORDS = list(make_coords_list())
 PLATE_SIZE = len(WELL_COORDS)
 
 SAMPLES_FILE_HEADERS = [
@@ -42,7 +39,6 @@ SAMPLES_FILE_HEADERS = [
 ]
 
 PRINT_FILE_HEADERS = ["barcode", "text"]
-
 BARACODA_PREFIX = "TEST"
 
 
@@ -95,8 +91,8 @@ def create_row(dt, well_index, result, barcode) -> str:
         create_rna_pcr_id(barcode, well_num, well_coordinate),
         result,
         create_test_timestamp(dt),
-        "AP","","","","","","","","","","","",""
-    ]
+        "AP",
+    ] + [""] * 12
 
 
 def create_plate_rows(dt, num_positives, plate_barcode):
