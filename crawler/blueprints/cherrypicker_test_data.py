7fc33354df0d10fd8bc13ddf80e730ae7bee084d
Stuart McHattie <sm49@sanger.ac.uk>
2021-07-13 11:22:03 +0100

    Remove flask-cors package and usage

--- crawler/blueprints/cherrypicker_test_data.py
+++ crawler/blueprints/cherrypicker_test_data.py
@@ -2,7 +2,6 @@ import logging
 from datetime import datetime, timezone
 
 from flask import Blueprint, request
-from flask_cors import CORS
 
 from crawler.constants import FIELD_STATUS_COMPLETED, FLASK_ERROR_MISSING_PARAMETERS, FLASK_ERROR_UNEXPECTED
 from crawler.helpers.responses import bad_request, internal_server_error, ok
@@ -12,7 +11,6 @@ from crawler.types import FlaskResponse
 logger = logging.getLogger(__name__)
 
 bp = Blueprint("cherrypicker", __name__)
-CORS(bp)
 
 
 @bp.post("/cherrypick-test-data")
