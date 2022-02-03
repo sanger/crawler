2b11744885bd8da2a155507df0f54b452904bbcb
Stuart McHattie <sm49@sanger.ac.uk>
2022-02-03 10:18:26 +0000

    Reformat schema files with black

--- crawler/schema/consumer.py
+++ crawler/schema/consumer.py
@@ -1,25 +1,27 @@
 import pika
 from time import sleep
 
+
 def callback(ch, method, properties, body):
     if body:
         print(f"Doing something with the message: {body}.")
         sleep(1)
         print(f"I did something with the message: {body}.")
-    else: 
+    else:
         print("I'm not working :(")
+
 
 if __name__ == "__main__":
 
     print("Starting to receive...")
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
     channel = connection.channel()
 
     queue = "sample-messenger-test"
-    channel.queue_declare(queue = queue)
+    channel.queue_declare(queue=queue)
 
-    channel.basic_consume(queue = queue, on_message_callback = callback, auto_ack = True)
-    channel.start_consuming() # nom nom
+    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()  # nom nom
 
     print("Finished consuming.")
--- crawler/schema/producer.py
+++ crawler/schema/producer.py
@@ -2,14 +2,14 @@ import pika
 
 if __name__ == "__main__":
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
     channel = connection.channel()
 
     queue = "sample-messenger-test"
-    channel.queue_declare(queue = queue)
+    channel.queue_declare(queue=queue)
 
     message = "hello this is a message"
-    channel.basic_publish(exchange = '', routing_key = queue, body = message)
+    channel.basic_publish(exchange="", routing_key=queue, body=message)
 
     print(f"Sent the message: '{message}'.")
 
