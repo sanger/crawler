3d2d4defbbc97759375ce5aaa30830cc7a18694d
Stuart McHattie <sm49@sanger.ac.uk>
2022-02-03 10:42:57 +0000

    Fix remaining issues in producer linting

--- schema_poc/producer.py
+++ schema_poc/producer.py
@@ -1,15 +1,15 @@
-import pika
+from pika import BlockingConnection, ConnectionParameters
 
 if __name__ == "__main__":
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    connection = BlockingConnection(ConnectionParameters("localhost"))
     channel = connection.channel()
 
     queue = "sample-messenger-test"
     channel.queue_declare(queue=queue)
 
     message = "hello this is a message"
-    channel.basic_publish(exchange="", routing_key=queue, body=message)
+    channel.basic_publish(exchange="", routing_key=queue, body=message.encode("utf-8"))
 
     print(f"Sent the message: '{message}'.")
 
