cb41fb97979c6948d95ff4d1bd2260be5edab81a
Stuart McHattie <sm49@sanger.ac.uk>
2022-02-04 10:18:13 +0000

    Use StringIO for AVRO JSON

--- schema_poc/test_avro.py
+++ schema_poc/test_avro.py
@@ -1,18 +1,19 @@
 import json
 from datetime import datetime
-from io import BytesIO
+from io import StringIO
 
 from fastavro import json_reader, json_writer, parse_schema
 
 
 def datetime_to_millis(dt):
-  return (int)(dt.timestamp() * 1000)
+    return (int)(dt.timestamp() * 1000)
+
 
 def millis_to_datetime(millis):
-  return datetime.fromtimestamp(millis / 1000)
+    return datetime.fromtimestamp(millis / 1000)
 
 
-samples = [{
+sample1 = {
     "labId": "CPTD",
     "sampleUuid": "UUID-123456-01",
     "plateBarcode": "BARCODE001",
@@ -23,14 +24,21 @@ samples = [{
     "fitToPick": True,
     "testedDateUtc": datetime_to_millis(datetime(2022, 2, 1, 13, 45, 8)),
     "messageCreateDateUtc": datetime_to_millis(datetime.utcnow()),
-}]
+}
+
+samples = [sample1, sample1]
 
 with open("plate_map_sample_v1.avsc", "r") as schema_file:
-  schema = parse_schema(json.load(schema_file))
+    schema = parse_schema(json.load(schema_file))
 
-with open("plate_map_samples_v1.avro", "w") as out_file:
-  json_writer(out_file, schema, samples)
+string_writer = StringIO()
+json_writer(string_writer, schema, samples)
 
-with open("plate_map_samples_v1.avro", "r") as in_file:
-  for sample in json_reader(in_file, schema):
+message_json = string_writer.getvalue()
+
+# Send to RabbitMQ at this point
+# Let's now assume we just read raw_bytes from RabbitMQ
+
+string_reader = StringIO(message_json)
+for sample in json_reader(string_reader, schema):
     print(sample)
