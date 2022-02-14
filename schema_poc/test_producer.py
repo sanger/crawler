f3265ccfde609ecb12fddfe52d0f5e4c73fbd2f8
Stuart McHattie <sm49@sanger.ac.uk>
2022-02-14 16:32:10 +0000

    Update comment about the MESSAGES object

--- schema_poc/test_producer.py
+++ schema_poc/test_producer.py
@@ -8,7 +8,7 @@ from test_messages import MESSAGES
 # The test here assume RedPanda is running on local host port 8081, which it will be if you used the dependencies
 # Docker Compose file.  Then you need to have inserted the schema for this message type.  This can be done with PostMan
 # by creating a POST request to http://localhost:8081/subjects/{subject_name}/versions with the JSON body shown.
-# Replace {subject_name} in the URL with the intended subject name.  See the keys on MESSAGES below.
+# Replace {subject_name} in the URL with the intended subject name.  See the keys on the MESSAGES object.
 # Replace the schema definition with a slash escaped string based on the relevant .avsc file in this directory.
 # {
 #     "schema": "slash_escaped_string_of_schema_json"
