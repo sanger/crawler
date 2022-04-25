a6be6abb3e842048f745419f3adce2c7329978fe
Stuart McHattie <sm49@sanger.ac.uk>
2022-04-25 17:22:04 +0100

    Add tests for RabbitMessageProcessor

--- crawler/rabbit/schema_registry.py
+++ crawler/rabbit/schema_registry.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from requests import get
 
+from crawler.exceptions import TransientRabbitError
+
 RESPONSE_KEY_VERSION = "version"
 RESPONSE_KEY_SCHEMA = "schema"
 
@@ -13,12 +15,15 @@ class SchemaRegistry:
 
     def get_schema(self, subject: str, version_num: Union[str, int]) -> dict:
         # TODO: Need to add caching
-        return (dict)(
-            get(
-                f"{self._base_uri}/subjects/{subject}/versions/{version_num}",
-                headers={"X-API-KEY": self._api_key},
-            ).json()
-        )
+        try:
+            return (dict)(
+                get(
+                    f"{self._base_uri}/subjects/{subject}/versions/{version_num}",
+                    headers={"X-API-KEY": self._api_key},
+                ).json()
+            )
+        except Exception:
+            raise TransientRabbitError(f"Unable to connect to schema registry at {self._base_uri}")
 
     def get_latest_schema(self, subject: str) -> dict:
         return self.get_schema(subject, "latest")
