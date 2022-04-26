7bdb500c1e224fd064da6cb6f86f84e18a6afc95
Stuart McHattie <sm49@sanger.ac.uk>
2022-04-26 12:40:24 +0100

    Add tests for AvroEncoder

--- crawler/rabbit/avro_encoder.py
+++ crawler/rabbit/avro_encoder.py
@@ -2,7 +2,7 @@ import json
 from io import StringIO
 from typing import Any, List, NamedTuple
 
-from fastavro import json_reader, json_writer, parse_schema
+import fastavro
 
 from crawler.rabbit.schema_registry import RESPONSE_KEY_SCHEMA, RESPONSE_KEY_VERSION
 
@@ -19,13 +19,13 @@ class AvroEncoder:
 
     def _schema_response(self, version):
         if version is None:
-            return self._schema_registry.get_latest_schema(self._subject)
+            return self._schema_registry.get_schema(self._subject)
         else:
             return self._schema_registry.get_schema(self._subject, version)
 
     def _schema(self, schema_response):
         schema_obj = json.loads(schema_response[RESPONSE_KEY_SCHEMA])
-        return parse_schema(schema_obj)
+        return fastavro.parse_schema(schema_obj)
 
     def _schema_version(self, schema_response):
         return schema_response[RESPONSE_KEY_VERSION]
@@ -33,12 +33,14 @@ class AvroEncoder:
     def encode(self, records: List, version: str = None) -> EncodedMessage:
         schema_response = self._schema_response(version)
         string_writer = StringIO()
-        json_writer(string_writer, self._schema(schema_response), records)
+        fastavro.json_writer(string_writer, self._schema(schema_response), records)
 
-        return EncodedMessage(body=string_writer.getvalue().encode(), version=self._schema_version(schema_response))
+        return EncodedMessage(
+            body=string_writer.getvalue().encode(), version=str(self._schema_version(schema_response))
+        )
 
     def decode(self, message: bytes, version: str) -> Any:
         schema_response = self._schema_response(version)
         string_reader = StringIO(message.decode("utf-8"))
 
-        return json_reader(string_reader, self._schema(schema_response))
+        return fastavro.json_reader(string_reader, self._schema(schema_response))
