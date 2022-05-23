959c404aad865cf74a821b28014060b59230bdb9
Stuart McHattie <sm49@sanger.ac.uk>
2022-05-23 18:16:44 +0100

    Add a transaction to the MongoDB export

--- crawler/processing/create_plate_exporter.py
+++ crawler/processing/create_plate_exporter.py
@@ -29,19 +29,25 @@ class CreatePlateExporter:
         self._samples_inserted = 0
 
     def export_to_mongo(self):
-        try:
-            source_plate_result = self._record_source_plate_in_mongo_db()
+        with self._mongo_db.client.start_session() as session:
+            try:
+                with session.start_transaction():
+                    source_plate_result = self._record_source_plate_in_mongo_db(session)
 
-            if not source_plate_result.success:
-                self._message.add_error(source_plate_result.create_plate_error)
-                return
+                    if not source_plate_result.success:
+                        self._message.add_error(source_plate_result.create_plate_error)
+                        session.abort_transaction()
+                        return
 
-            samples_result = self._record_samples_in_mongo_db()
+                    samples_result = self._record_samples_in_mongo_db(session)
 
-            if not samples_result.success:
-                self._message.add_error(samples_result.create_plate_error)
-        finally:
-            self._mongo_db.client.close()
+                    if not samples_result.success:
+                        self._message.add_error(samples_result.create_plate_error)
+                        session.abort_transaction()
+
+                    session.commit_transaction()
+            finally:
+                self._mongo_db.client.close()
 
     def export_to_dart(self):
         try:
@@ -80,14 +86,17 @@ class CreatePlateExporter:
 
         return self.__mongo_db
 
-    def _record_source_plate_in_mongo_db(self) -> ExportResult:
+    def _record_source_plate_in_mongo_db(self, session) -> ExportResult:
         """Find an existing plate in MongoDB or add a new one for the plate in the message."""
         try:
             plate_barcode = self._message.plate_barcode.value
             lab_id_field = self._message.lab_id
 
-            source_plates_collection = get_mongo_collection(self._mongo_db, COLLECTION_SOURCE_PLATES)
-            mongo_plate = source_plates_collection.find_one(filter={MONGO_PLATE_BARCODE: plate_barcode})
+            session_database = get_mongo_db(self._config, session.client)
+            source_plates_collection = get_mongo_collection(session_database, COLLECTION_SOURCE_PLATES)
+            mongo_plate = source_plates_collection.find_one(
+                filter={MONGO_PLATE_BARCODE: plate_barcode}, session=session
+            )
 
             if mongo_plate is not None:
                 # There was a plate in Mongo DB for this field barcode so check that the lab ID matches then return.
@@ -111,7 +120,7 @@ class CreatePlateExporter:
 
             # Create a new plate for this message.
             mongo_plate = create_source_plate_doc(plate_barcode, lab_id_field.value)
-            source_plates_collection.insert_one(mongo_plate)
+            source_plates_collection.insert_one(mongo_plate, session=session)
             self._plate_uuid = mongo_plate[FIELD_LH_SOURCE_PLATE_UUID]
 
             return ExportResult(success=True, create_plate_error=None)
@@ -123,5 +132,5 @@ class CreatePlateExporter:
                 f"There was an error updating MongoDB while exporting plate with barcode '{plate_barcode}'."
             )
 
-    def _record_samples_in_mongo_db(self) -> ExportResult:
+    def _record_samples_in_mongo_db(self, session) -> ExportResult:
         return ExportResult(success=True, create_plate_error=None)
